88f018c1e835a6519490d5b0edd90c3c7da7aecf
alperbyldz <alperbyildiz@gmail.com>
2024-12-30 20:05:57 +0300

    Update heat.py

--- heat.py
+++ heat.py
@@ -7,7 +7,7 @@ COLD_DENSITY = 995.8175  # kg/m³
 COLD_VISCOSITY = 803.4/(10**6)  # N·s/m^2
 COLD_THERMAL_CONDUCTIVITY = 617.2/(10**3)  # W/(m·K)
 COLD_PRANDTL_NUMBER = 5.452
-#COLD_HEAT_TRANSFER_COEFF = 1  # we are going to calculate W/(m²·K)
+#COLD_HEAT_TRANSFER_COEFF = ?  # we are going to calculate W/(m²·K)
 #hot => mix
 HOT_TEMP_IN = 80  # °C
 HOT_TEMP_OUT = 50  # °C
@@ -17,7 +17,7 @@ HOT_DENSITY = 1008.8649 # kg/m³
 HOT_VISCOSITY = 5.94967/(10**4)  # N·s/m^2
 HOT_THERMAL_CONDUCTIVITY = 0.485208  # W/(m·K)
 HOT_PRANDTL_NUMBER = 4.54 # unitless
-#HOT_HEAT_TRANSFER_COEFF = 1  # we are going to calculate W/(m²·K)
+#HOT_HEAT_TRANSFER_COEFF = ?  # we are going to calculate W/(m²·K)
 
 tube_diameters = [(0.0159, 0.0004572), (0.0191, 0.0004572),
                   (0.0222, 0.000381),(0.0254, 0.0004064),
@@ -44,7 +44,6 @@ LMDT = (delta_T_1-delta_T_2)/ math.log((delta_T_1/delta_T_2), math.e) #celcius
 average_temp_cold = (COLD_TEMP_IN+COLD_TEMP_OUT)/2 #celcius
 average_temp_hot = (HOT_TEMP_IN+HOT_TEMP_OUT)/2 #celcius
 
-
 def area_for_cross_flow_5(tube_outer_diameter,shell_diameter):
     tube_pitch = tube_outer_diameter*1.25
     buffle_space = shell_diameter*0.6
@@ -60,57 +59,71 @@ def flow_area_per_tube_7(tube_inner_diameter):
     return a_t
 
 def area_for_baffle_window_8(tube_number,shell_diameter,tube_outer_diameter):
-    F_b = 0.1955 # optionel
+    F_b = 1 # 0.1955 # optionel
     N_b = tube_number/4 # must be integer
     battle_window_area = F_b*((math.pi*(shell_diameter**2))/4)-N_b*((math.pi*(tube_outer_diameter**2))/4)
+
     return battle_window_area
+
 def Gb_calculator_9( tube_number,shell_diameter,tube_outer_diameter):
     battle_window_area = area_for_baffle_window_8(tube_number,shell_diameter,tube_outer_diameter)
     Gb = W_HOT_MASS_FLOW/battle_window_area
     return Gb
+
 def flow_area_per_pass_10(tube_number,tube_inner_diameter):
     a_t = flow_area_per_tube_7(tube_inner_diameter)
     n_t = 1 # number of tube passs
     flow_area_per_pass = (tube_number*a_t)/n_t
     return flow_area_per_pass
+
 def mass_velocity_Ge_calculator_11(tube_outer_diameter,shell_diameter,tube_number):
     Gc = Gc_calculator_6(tube_outer_diameter,shell_diameter)
     Gb= Gb_calculator_9( tube_number,shell_diameter,tube_outer_diameter)
     Ge = (Gc*Gb)**(1/2)
     return Ge
+
 def average_velocity_calculator_12(tube_number,tube_inner_diameter,tube_outer_diameter,shell_diameter):
     A_t = flow_area_per_pass_10(tube_number,tube_inner_diameter)
     average_velocity = W_COLD_MASS_FLOW/(COLD_DENSITY*A_t)
     return average_velocity
+
 def Reynolds_s_calculator_13(tube_outer_diameter):
     Ge = mass_velocity_Ge_calculator_11(tube_outer_diameter,shell_diameter,tube_number)
     Reynolds_s = (tube_outer_diameter*Ge)/HOT_VISCOSITY
     return Reynolds_s
+
 def Reynolds_t_calculator_14(tube_number,tube_inner_diameter,tube_outer_diameter,shell_diameter):
     v = average_velocity_calculator_12(tube_number,tube_inner_diameter,tube_outer_diameter,shell_diameter)
     Reynolds_t = (COLD_DENSITY*v*tube_inner_diameter)/COLD_VISCOSITY
     return Reynolds_t
+
 def Pr_s_calculator_15():
     Pr_s = (Cp_HOT*HOT_VISCOSITY)/HOT_THERMAL_CONDUCTIVITY
     return Pr_s
+
 def Pr_t_calculator_16():
     Pr_t = (Cp_COLD*COLD_VISCOSITY)/COLD_THERMAL_CONDUCTIVITY
     return Pr_t
+
 def fi_s_calculator_17():
     fi_s = (HOT_VISCOSITY/COLD_VISCOSITY)**(0.14)
     return fi_s
+
 def Hs_calculator_with_donohue_18(tube_outer_diameter,shell_diameter,tube_number):
     Ge = mass_velocity_Ge_calculator_11(tube_outer_diameter,shell_diameter,tube_number)
     fi_s = fi_s_calculator_17()
     Hs = (0.2*(((tube_outer_diameter*Ge)/HOT_VISCOSITY)**(0.6))*((Cp_HOT*HOT_VISCOSITY)/HOT_THERMAL_CONDUCTIVITY)**(0.33))*((HOT_THERMAL_CONDUCTIVITY)/tube_outer_diameter*fi_s)
     return Hs
+
 def Ho_corrected_coef_calculator_19(tube_outer_diameter,shell_diameter,tube_number):
     Ho = Hs_calculator_with_donohue_18(tube_outer_diameter,shell_diameter,tube_number)
     return Ho
+
 def design_overall_coeff_calculator_21(tube_number,tube_lenght,tube_outer_diameter):
     area = math.pi*tube_lenght*tube_outer_diameter*tube_number
     U_d = Q_owerall/(area*LMDT)
     return U_d
+
 def Gnielinski_A_calculator_23(tube_number,tube_inner_diameter,tube_outer_diameter,shell_diameter):
     #epsilone/D = zero => clean pipe
     Re_t = Reynolds_t_calculator_14(tube_number,tube_inner_diameter,tube_outer_diameter,shell_diameter)
@@ -124,6 +137,7 @@ def Gnielinski_A_calculator_23(tube_number,tube_inner_diameter,tube_outer_diamet
             return A, Re_t, Re_d
     else:
         return -1,-1,-1
+
 def Gnielinski_f_calculator_24(tube_number,tube_inner_diameter,tube_outer_diameter,shell_diameter):
     A, Re_t, Re_d = Gnielinski_A_calculator_23(tube_number,tube_inner_diameter,tube_outer_diameter,shell_diameter)
     if A==-1:
@@ -131,6 +145,7 @@ def Gnielinski_f_calculator_24(tube_number,tube_inner_diameter,tube_outer_diamet
     else:
         f = (1/(-4*math.log(((-5.0452/Re_t)*math.log(A,10)),10)))**2
         return f, Re_d
+
 def Gnielinski_Nu_calculator_25(tube_number,tube_inner_diameter,tube_outer_diameter,shell_diameter):
     Pr_t = Pr_t_calculator_16()
     if 0.5 <= Pr_t <= 2000:
@@ -142,6 +157,7 @@ def Gnielinski_Nu_calculator_25(tube_number,tube_inner_diameter,tube_outer_diame
             return -1
     else:
         return -1
+
 def Hi_calculator_with_donohue_26(tube_number,tube_inner_diameter,tube_outer_diameter,shell_diameter):
     Nu = Gnielinski_Nu_calculator_25(tube_number,tube_inner_diameter,tube_outer_diameter,shell_diameter)
     if Nu != -1:
@@ -149,6 +165,7 @@ def Hi_calculator_with_donohue_26(tube_number,tube_inner_diameter,tube_outer_dia
         return Hi
     else:
         return -1
+
 def H_io_calculator_27(tube_number,tube_inner_diameter,tube_outer_diameter,shell_diameter):
     Hi = Hi_calculator_with_donohue_26(tube_number,tube_inner_diameter,tube_outer_diameter,shell_diameter)
     if Hi !=-1:
@@ -156,6 +173,7 @@ def H_io_calculator_27(tube_number,tube_inner_diameter,tube_outer_diameter,shell
         return h_io
     else:
         return -1
+
 def clean_overall_coeff_calculator_20(tube_inner_diameter, tube_outer_diameter,shell_diameter,tube_number):
     ho = Ho_corrected_coef_calculator_19(tube_outer_diameter,shell_diameter,tube_number)
     h_io = H_io_calculator_27(tube_number,tube_inner_diameter,tube_outer_diameter,shell_diameter)
@@ -164,6 +182,7 @@ def clean_overall_coeff_calculator_20(tube_inner_diameter, tube_outer_diameter,s
         return U_c
     else:
         return -1
+
 def dirt_factor_calculator_22(tube_number,tube_lenght,tube_outer_diameter,tube_inner_diameter,shell_diameter):
     U_d = design_overall_coeff_calculator_21(tube_number,tube_lenght,tube_outer_diameter)
     U_c = clean_overall_coeff_calculator_20(tube_inner_diameter, tube_outer_diameter,shell_diameter,tube_number)
@@ -172,6 +191,7 @@ def dirt_factor_calculator_22(tube_number,tube_lenght,tube_outer_diameter,tube_i
         return R_d
     else:
         return -1
+
 #pressure drop calculations
 def Reynolds_number_calculator_28(tube_outer_diameter,shell_diameter):
     tube_pitch = tube_outer_diameter*1.25
@@ -180,6 +200,7 @@ def Reynolds_number_calculator_28(tube_outer_diameter,shell_diameter):
     Gc = Gc_calculator_6(tube_outer_diameter,shell_diameter)
     Re = (D_h*Gc)/HOT_VISCOSITY
     return Re ,D_h , Gc
+
 def f_calculator_with_Re_29(tube_outer_diameter,shell_diameter):
     Re, D_h , Gc= Reynolds_number_calculator_28(tube_outer_diameter,shell_diameter)
     if Re >= 400:
@@ -187,10 +208,12 @@ def f_calculator_with_Re_29(tube_outer_diameter,shell_diameter):
         return f , D_h , Gc
     else :
         return -1 , -1 , -1
+
 def number_of_crosses_30(shell_diameter,lenght):
     Buffle_spacing = 0.6*shell_diameter
     N_plus_1 = lenght/Buffle_spacing
     return N_plus_1
+
 def inside_tube_t_delta_P_calc_31(lenght,tube_inner_diameter,tube_number,tube_outer_diameter,shell_diameter):
     v = average_velocity_calculator_12(tube_number,tube_inner_diameter,tube_outer_diameter,shell_diameter)
     f , D_h , Gc = f_calculator_with_Re_29(tube_outer_diameter,shell_diameter)
@@ -199,10 +222,12 @@ def inside_tube_t_delta_P_calc_31(lenght,tube_inner_diameter,tube_number,tube_ou
         return delta_P_t
     else:
         return -1
+
 def tube_side_r_return_delta_P_calc_32( tube_number, tube_inner_diameter,tube_outer_diameter, shell_diameter):
     v = average_velocity_calculator_12(tube_number,tube_inner_diameter,tube_outer_diameter,shell_diameter)
     delta_P_r = 4*tube_number*(COLD_DENSITY*(v**2)/2)
     return delta_P_r
+
 def total_pressure_drop_calculator(lenght,tube_inner_diameter,tube_number,tube_outer_diameter,shell_diameter):
     delta_P_t = inside_tube_t_delta_P_calc_31(lenght,tube_inner_diameter,tube_number,tube_outer_diameter,shell_diameter)
     if delta_P_t == -1:
@@ -211,6 +236,7 @@ def total_pressure_drop_calculator(lenght,tube_inner_diameter,tube_number,tube_o
         delta_P_r = tube_side_r_return_delta_P_calc_32( tube_number, tube_inner_diameter,tube_outer_diameter, shell_diameter)
         total_drop = delta_P_t + delta_P_r
         return total_drop
+
 def delta_P_s_drop_calculator_34(tube_outer_diameter,shell_diameter,lenght):
     f ,D_h ,Gc = f_calculator_with_Re_29(tube_outer_diameter,shell_diameter)
     if f == 1 :
@@ -220,12 +246,14 @@ def delta_P_s_drop_calculator_34(tube_outer_diameter,shell_diameter,lenght):
         fi_s =  fi_s_calculator_17()
         delta_P_s_drop = (f*(Gc**2)*shell_diameter*N_plus_one)/(2*D_h*fi_s*HOT_DENSITY)
         return delta_P_s_drop
+
 combinations = []
 for length, shell_diameter in lengths_and_shell_diameter:
     for outer_diameter, thickness in tube_diameters:
         inner_diameter = outer_diameter - thickness
         max_of_tubes = max_number_of_tubes(outer_diameter,shell_diameter)
         combinations.append((outer_diameter, inner_diameter, length, shell_diameter,max_of_tubes))
+
 results = []
 for outer_diameter, inner_diameter, length, shell_diameter,max_of_tubes in combinations:
     for i in range(1,max_of_tubes+1):
@@ -238,7 +266,81 @@ for outer_diameter, inner_diameter, length, shell_diameter,max_of_tubes in combi
                     results.append((outer_diameter, shell_diameter, i, length, total_P_drop, delta_P_s))
             else:
                 pass
+
 results.sort(key=lambda x: x[4])
+sayac = 1
 for outer_diameter, shell_diameter, i, length, total_P_drop, delta_P_s in results:
-   print("Outer dia: {} m , Shell dia: {} m , Total Tube Number: {}, Lenght: {} m , delta P total: {} Pa, Delta Ps: {} Pa".format(outer_diameter, shell_diameter, i, length, total_P_drop, delta_P_s))
-print(len(results))
+    if sayac <= 10:
+        print("Outer dia: {} m , Shell dia: {} m , Total Tube Number: {}, Lenght: {} m , delta P total: {} Pa, Delta Ps: {} Pa\n\n\n\n".format(outer_diameter, shell_diameter, i, length, total_P_drop, delta_P_s))
+        sayac += 1
+    else:
+        pass
+print("********************-----------------------------------------------------------------------------------------------------------------------********************")
+
+outer_diameter = 0.05080
+shell_diameter = 0.6128
+inner_diameter = 0.05080-0.0003302
+tube_number = 128
+length = 3.658
+
+cross_flow_area =area_for_cross_flow_5(outer_diameter,shell_diameter)
+print("Sc: ", cross_flow_area)
+Gc = Gc_calculator_6(outer_diameter,shell_diameter)
+print("Gc: ", Gc)
+a_t = flow_area_per_tube_7(inner_diameter)
+print("a_t: ", a_t)
+battle_window_area = area_for_baffle_window_8(tube_number,shell_diameter,outer_diameter)
+print("Sb: ", battle_window_area)
+Gb= Gb_calculator_9( tube_number,shell_diameter,outer_diameter)
+print("Gb: ", Gb)
+flow_area_per_pass = flow_area_per_pass_10(tube_number,inner_diameter)
+print("A_t: ",flow_area_per_pass)
+Ge = mass_velocity_Ge_calculator_11(outer_diameter,shell_diameter,tube_number)
+print("Ge: ", Ge)
+average_velocity = average_velocity_calculator_12(tube_number,inner_diameter,outer_diameter,shell_diameter)
+print("v : ", average_velocity)
+Reynolds_s = Reynolds_s_calculator_13(outer_diameter)
+print("Reynolds_s: ", Reynolds_s)
+Reynolds_t = Reynolds_t_calculator_14(tube_number,inner_diameter,outer_diameter,shell_diameter)
+print("Reynolds_t: ", Reynolds_t)
+Pr_s = Pr_s_calculator_15()
+print("Pr_s: ", Pr_s)
+Pr_t = Pr_t_calculator_16()
+print("Pr_t: ", Pr_t)
+fi_s = fi_s_calculator_17()
+print("fi_s: ", fi_s)
+Hs = Hs_calculator_with_donohue_18(outer_diameter,shell_diameter,tube_number)
+print("Hs and Ho: ", Hs)
+U_d = design_overall_coeff_calculator_21(tube_number,length,outer_diameter)
+print("U_d: ", U_d)
+A , Re_t , Re_d = Gnielinski_A_calculator_23(tube_number,inner_diameter,outer_diameter,shell_diameter)
+print("A: ", A)
+print("Re_t: ", Re_t)
+print("Re_d: ", Re_d)
+f , red = Gnielinski_f_calculator_24(tube_number,inner_diameter,outer_diameter,shell_diameter)
+print("Gnielinski f: ", f)
+Nu = Gnielinski_Nu_calculator_25(tube_number,inner_diameter,outer_diameter,shell_diameter)
+print("Nu: ",Nu)
+Hi = Hi_calculator_with_donohue_26(tube_number,inner_diameter,outer_diameter,shell_diameter)
+print("Hi: ", Hi)
+H_io = H_io_calculator_27(tube_number,inner_diameter,outer_diameter,shell_diameter)
+print("Hio: " , H_io)
+U_c = clean_overall_coeff_calculator_20(inner_diameter, outer_diameter,shell_diameter,tube_number)
+print("U_c: ", U_c)
+R_d = dirt_factor_calculator_22(tube_number,length,outer_diameter,inner_diameter,shell_diameter)
+print("R_d: ", R_d)
+print("***** Pressure part *******")
+Re , ab, cd = Reynolds_number_calculator_28(outer_diameter,shell_diameter)
+print("Re: ", Re)
+f , D_h, zy  = f_calculator_with_Re_29(outer_diameter,shell_diameter)
+print("f for pressure ",f)
+print("D_h for pressure ",D_h)
+N_plus_1 = number_of_crosses_30(shell_diameter,length)
+print("N_plus_1: ", N_plus_1)
+delta_P_t = inside_tube_t_delta_P_calc_31(length,inner_diameter,tube_number,outer_diameter,shell_diameter)
+print("delta_P_t: ", delta_P_t)
+delta_P_r = tube_side_r_return_delta_P_calc_32( tube_number, inner_diameter,outer_diameter, shell_diameter)
+print("delta_P_r: ", delta_P_r)
+print("Total P drop: ", delta_P_t+delta_P_r)
+delta_P_s_drop = delta_P_s_drop_calculator_34(outer_diameter,shell_diameter,length)
+print("delta_P_s_drop: ", delta_P_s_drop)
